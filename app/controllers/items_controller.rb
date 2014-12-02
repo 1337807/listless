209254c794f281558be352f175a2bfd250b0bb2f
Jonan Scheffler <jonanscheffler@gmail.com>
2014-12-01 23:28:29 -0800

    Create and edit items.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,19 +4,33 @@ class ItemsController < ApplicationController
   end
 
   def create
-    penny_params = item_params
-    penny_params[:price] = Item.pennies_from_price(penny_params[:price])
-
-    if item = List.find(params[:list_id]).items.create(penny_params)
+    if item = List.find(params[:list_id]).items.create(item_params)
       redirect_to list_path(item.list)
     else
       render :new
     end
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update_attributes(item_params)
+    redirect_to list_path(item.list)
+  end
+
+  def destroy
+    item = Item.find(params[:id]).destroy
+    redirect_to list_path(item.list)
+  end
+
   private
 
   def item_params
-    params.require(:item).permit(:name, :url, :price, :list_id)
+    permitted_params = params.require(:item).permit(:name, :url, :price, :list_id)
+    permitted_params[:price] = Item.pennies_from_price(permitted_params[:price])
+    permitted_params
   end
 end
