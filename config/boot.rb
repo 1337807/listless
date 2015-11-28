b6fddba58b0cf2fc799dcb2ed097f1249bf07cef
Jonan Scheffler <jonan@heroku.com>
2015-11-28 14:14:41 -0800

    Trying to debug opaque R10 errors on Heroku.

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,24 @@
+if ENV['DEBUG_REQUIRE']
+  require 'benchmark'
+
+  def require(file)
+    @first ||= Time.now
+
+    required = nil
+    elapsed = Benchmark.measure do
+      required = super
+    end
+
+    if ENV['DEBUG_REQUIRE'].to_f < elapsed.total
+      total = elapsed.format("%t require #{file}")
+      from_start = (Time.now - @first).to_i
+      $stdout.puts "#{total} (#{from_start} second(s) from start)"
+    end
+
+    required
+  end
+end
+
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
